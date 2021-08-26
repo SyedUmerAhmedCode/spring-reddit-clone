c0b58b2c9c4e51acff9003bc7d5fe1577f702fd0
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-08-26 14:36:49 +0200

    Feature: Adapted application configuration properties.

--- src/main/java/com/umer/springredditclone/repository/VoteRepository.java
+++ src/main/java/com/umer/springredditclone/repository/VoteRepository.java
@@ -1,11 +1,7 @@
 package com.umer.springredditclone.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.umer.springredditclone.model.Post;
-import com.umer.springredditclone.model.User;
 import com.umer.springredditclone.model.Vote;
 
 public interface VoteRepository extends JpaRepository<Vote, Long>{
