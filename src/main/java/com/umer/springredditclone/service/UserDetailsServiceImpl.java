1111f8f116eb5856c72866a0b69dd76c9a75cc57
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-09-10 18:04:19 +0200

    Feature: Implemented UserDetailsService implementation.

--- src/main/java/com/umer/springredditclone/service/UserDetailsServiceImpl.java
+++ src/main/java/com/umer/springredditclone/service/UserDetailsServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.umer.springredditclone.exceptions.SpringRedditException;
 import com.umer.springredditclone.model.User;
@@ -18,19 +19,22 @@ import com.umer.springredditclone.repository.UserRepository;
 
 import lombok.AllArgsConstructor;
 
+@Service
 @AllArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+	private static final String NO_USER_FOUND_WITH_NAME = "No user found with name: ";
+	private static final String USER = "USER";
 	private final UserRepository userRepository;
 
 	@Override
 	@Transactional(/* readOnly=true */)
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		final Optional<User> userOptional = userRepository.findByUsername(username);
-		User user = userOptional.orElseThrow(() -> new SpringRedditException("No user found with name: " + username));
+		User user = userOptional.orElseThrow(() -> new SpringRedditException(NO_USER_FOUND_WITH_NAME + username));
 
 		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
-				user.isEnabled(), true, true, true, getAuthorities("USER"));
+				user.isEnabled(), true, true, true, getAuthorities(USER));
 	}
 
 	private Collection<? extends GrantedAuthority> getAuthorities(String role) {
