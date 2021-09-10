7bcea53dcb63b0bbbc4adf52fcf13619db41990f
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-09-10 17:23:27 +0200

    Feature: Test commit and push.

--- src/main/java/com/umer/springredditclone/config/SecurityConfig.java
+++ src/main/java/com/umer/springredditclone/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
 				.anyRequest() 
 				.authenticated();
-	}
+	}  
 
 	@Bean
 	PasswordEncoder passwordEncoder() {
