d8f929a3e2185fb168c03cdcee31301e591db7de
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-09-11 08:29:30 +0200

    Feature: Created scaffolding for the JWT service.

--- src/main/java/com/umer/springredditclone/security/JwtProvider.java
+++ src/main/java/com/umer/springredditclone/security/JwtProvider.java
@@ -1,5 +1,21 @@
 package com.umer.springredditclone.security;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+
+@Service
 public class JwtProvider {
 
+	public String generateToken(Authentication authentication) {
+		org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) authentication
+				.getPrincipal();
+		return Jwts.builder()
+				.setSubject(principal.getUsername())
+//				.signWith(getPrivateKey())
+				.compact();
+				
+		
+	}
 }
